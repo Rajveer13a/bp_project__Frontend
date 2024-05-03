5251b93a91d6e445dc5052db02622b8c409aeb69
Rajveer13a <soulrpr123@gmail.com>
2024-05-03 14:57:12 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+
+
+  return (
+    <>
+
+      <div className="flex item justify-center items-center h-[80px] bg-background text-foreground">
+        <Button>basic setup</Button>
+
+      </div>
+    </>
+  )
+}
+
+export default App
