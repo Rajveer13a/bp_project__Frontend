6527040ad5286f8601250b5d1df3b30297c777ba
Rajveer13a <soulrpr123@gmail.com>
2024-05-05 13:48:40 +0530

    working on navbar

--- src/components/HoverMenu.jsx
+++ src/components/HoverMenu.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function HoverMenu({title, list, children}) {
+function HoverMenu({title, list, children, className}) {
   return (
-    <div className='text-[14.5px]'>
+    <div className={` ${className} `}>
       
       <div className="dropdown dropdown-hover">
-  <div tabIndex={0} role="button" className="m-1 hover:text-blue-600"> {title || "menu"} </div>
+  <div tabIndex={0} role="button" className="m-1 whitespace-nowrap hover:text-blue-600"> {title || "menu"} </div>
   <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 ">
 
     {list && list.map(
