171455775b2b3cfdf11eb386e918a0e224cba089
Rajveer13a <soulrpr123@gmail.com>
2025-02-22 15:04:40 +0530

    feat: add Vercel Analytics integration to track application performance

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { Route, Routes, useNavigate } from "react-router-dom"
 import { v4 as uuidv4 } from 'uuid';
-
+import { Analytics } from '@vercel/analytics/react';
 import eventEmitter from "./Helpers/eventEmitter"
 import LearnLecture from './Pages/LearnLecture';
 import AuthProtectedRoutes from './ProtectedRoutes/AuthProtectedRoutes';
@@ -45,7 +45,7 @@ function App() {
 
     <Routes>
 
-
+      <Analytics />
       <Route element={<RedirectManagementToRoutes />}>
 
         <Route element={<AuthProtectedRoutes />}>
